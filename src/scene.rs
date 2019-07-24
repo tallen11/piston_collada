60d192cec1b5f9829c0e0ed655d5d17dec7540b3
Tate Allen <tatea7@yahoo.com>
2019-07-23 22:57:32 -0400

    Added Clone derive to camera and light

--- src/scene.rs
+++ src/scene.rs
@@ -1,11 +1,11 @@
 use vecmath::Matrix4;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Camera {
     pub transform: Matrix4<f32>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Light {
     pub transform: Matrix4<f32>,
 }
